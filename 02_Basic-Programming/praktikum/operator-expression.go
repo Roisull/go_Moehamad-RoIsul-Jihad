73674150cdddd98473300ef22e19bb8616613a75
Roisull <lusiro2001@students.ac.id>
2023-08-24 10:16:37 +0700

    praktikum operator expression

--- 02_Basic-Programming/praktikum/operator-expression.go
+++ 02_Basic-Programming/praktikum/operator-expression.go
@@ -6,4 +6,20 @@ func main() {
 	// operand dan operator
 	x := 1 + 2
 	fmt.Println(x)
+
+	// expression
+	a := 5
+	b := 6
+	c := a + b
+	fmt.Println(c)
+
+	// LUAS SEGITIGA
+	alas := 10
+	tinggi := 15
+	luas := (alas * tinggi) / 2
+	fmt.Println(luas)
+
+	// string operation
+	helloworld := "hello" + " " + "world"
+	fmt.Println(helloworld)
 }
