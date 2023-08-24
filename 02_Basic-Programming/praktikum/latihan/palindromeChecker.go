659634caa708f6688c93b190ac9db04d6c057639
Roisull <lusiro2001@students.ac.id>
2023-08-24 12:10:33 +0700

    latihan membuat program palindromeChecker

---
+++ 02_Basic-Programming/praktikum/latihan/palindromeChecker.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+func main() {
+    var input string
+    fmt.Println("Apakah palindrome?")
+    fmt.Print("Masukkan kata: ")
+
+    // Menggunakan Scanner untuk menangkap input dari konsol
+    fmt.Scanln(&input)
+    input = strings.ToLower(input) // Mengubah input menjadi huruf kecil
+    // Menghapus spasi dari input
+    input = strings.ReplaceAll(input, " ", "")
+    // Memeriksa apakah input adalah palindrome atau bukan
+    isPalindrome := true
+    for i := 0; i < len(input)/2; i++ {
+        if input[i] != input[len(input)-1-i] {
+            isPalindrome = false
+            break
+        }
+    }
+    // Menampilkan hasil
+    if isPalindrome {
+        fmt.Println("Captured:", input)
+        fmt.Println("Palindrome")
+    } else {
+        fmt.Println("Captured:", input)
+        fmt.Println("Bukan palindrome")
+    }
+}
