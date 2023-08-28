b9dc9cb5d9e4f7656127e987663c637ffc4aa6da
Roisull <lusiro2001@students.ac.id>
2023-08-28 16:32:23 +0700

    mengerjakan dan SS hasil dari soal prioritas 1 No 1

---
+++ 05_Data-Structure/praktikum/soalPrioritasNo1.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func ArrayMerge(arrayA, arrayB []string) []string {
+	merged := make(map[string]bool) // Membuat map untuk memastikan nilai unik
+	result := []string{}
+
+	for _, val := range arrayA {
+		if !merged[val] {
+			merged[val] = true
+			result = append(result, val)
+		}
+	}
+
+	for _, val := range arrayB {
+		if !merged[val] {
+			merged[val] = true
+			result = append(result, val)
+		}
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(ArrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
+	// ["king", "devil jin", "akuma", "eddie", "steve", "geese"]
+
+	fmt.Println(ArrayMerge([]string{"sergei", "jin"}, []string{"jin", "steve", "bryan"}))
+	// ["sergei", "jin", "steve", "bryan"]
+
+	fmt.Println(ArrayMerge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa", "law"}))
+	// ["alisa", "yoshimitsu", "devil jin", "law"]
+
+	fmt.Println(ArrayMerge([]string{}, []string{"devil jin", "sergei"}))
+	// ["devil jin", "sergei"]
+
+	fmt.Println(ArrayMerge([]string{"hwoarang"}, []string{}))
+	// ["hwoarang"]
+
+	fmt.Println(ArrayMerge([]string{}, []string{}))
+	// []
+}
