004fc66456d7fdfddb6e2af040c786fe3a169e9c
Roisull <lusiro2001@students.ac.id>
2023-11-21 06:44:25 +0700

    mengerjakan latihan praktikum beserta screenshots hasilnya dan summary tentang materi penerapan AI pada GOLANG

--- 28_Penggunaan_AI_pada_Golang/praktikum/main.go
+++ 28_Penggunaan_AI_pada_Golang/praktikum/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	fmt.Println("Starting")
+	router := gin.Default()
+
+	router.POST("recomendation", func(ctx *gin.Context) {
+		var input struct {
+			Budget float64 `json:"budget"`
+			Purpose string `json:"purpose"`
+		}
+
+		if err := ctx.BindJSON(&input); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Invalid input"})
+			return
+		}
+
+		// Berdasarkan purpose, tentukan rekomendasi laptop
+		var recomendation string
+		if input.Purpose == "gaming" {
+			recomendation = fmt.Sprintf("With a budget of Rp %.0f, you can get a high-performance gaming laptop that can handle the latest games and provide a smooth gaming experience. Here's a recommendation for a gaming laptop within your budget: ...", input.Budget)
+		} else {
+			recomendation = "Sorry, we currently don't have recommendations for the specified purpose."
+		}
+
+		response := gin.H{
+			"status": "success",
+			"data": recomendation,
+		}
+
+		ctx.JSON(http.StatusOK, response)
+	})
+
+	router.Run(":8080")
 }
---
+++ 28_Penggunaan_AI_pada_Golang/summary.md
@@ -0,0 +1,17 @@
+# materi penggunaan AI pada golang
+
+Penerapan AI sangat cocok pada bahasa GOLANG karena:
+1. GO dikembangkan langsung sama GOOGLE untuk pengembangan aplikasi server side dan berbasis cloud
+2. Go merupakan Compiled Language sehingga Lebih cepat di banding Interprated Language (ex: Python)
+3. Mendukung concurrency melalui fitur GOROUTINES sehingga dapat menajalankan banyak task secara simultan tanpa membebani memori
+4. Memiliki Standard Libraries yang sangat kaya, didukung oleh komunitas yang kuat
+5. Termasuk libraries untuk mendukung pengembangan A.I
+
+Beberapa LIBRARY untuk Machine Learning diantara yang populer adalah:
+1. GoLearn
+2. Gorgonia
+3. Gonum
+4. Gomind
+LIBRARY-LIBRARY ini memiliki kebutuhan masing-masing;
+
+Adapun cara yang lebih mudah untuk mengembangkan aplikasi berbasis A.I, yaitu menggunakan teknologi bernama A.I. as a Service (AlaaS). Yaitu sebuah Tools yang dapat segera digunakan. berguna bagi bisnis yang ingin menerapkan AI tanpa merekrut ahlinya dan tanpa mengeluarkan biaya yang relatif banyak.
